f817a52f719fc902185a1fd13916339f2c95ce49
Roma Commander <RomaCommander@yandex.ru>
2022-11-28 11:27:16 +0300

    end version

---
+++ index.js
@@ -0,0 +1,31 @@
+let popupOpenButton = document.querySelector('.profile__edit-button')
+let popup = document.querySelector('.popup')
+let popupCloseButton = document.querySelector('.popup__close')
+let popupSubmitButton = document.querySelector('.popup__submit')
+let profileName = document.querySelector('.profile__name')
+let profileJob = document.querySelector('.profile__job')
+let popupName = document.querySelector('.popup__item__el_name')
+let popupJob = document.querySelector('.popup__item__el_job')
+
+popupOpenButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  popup.classList.add('popup__opened')
+  if (profileName.textContent !== "") {
+    popupName.value = profileName.textContent;
+  }
+  if (profileJob !== "") {
+    popupJob.value = profileJob.textContent;
+  }
+})
+
+popupCloseButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  popup.classList.remove('popup__opened')
+})
+
+popupSubmitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('.profile__name').textContent = popupName.value;
+  document.querySelector('.profile__job').textContent = popupJob.value;
+  popup.classList.remove('popup__opened')
+})
